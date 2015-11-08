5c838c9c6b546254c2ce42e45d75c10d85a6b4bc
MilkVetch <457804000@qq.com>
2015-11-08 21:13:17 +1300

    fix animate scroll
    because of the fix menu
    something must change

--- js/main.js
+++ js/main.js
@@ -1,7 +1,61 @@
 // Description
 	console.log('Designed By: Xing.Huang');
-// start-arrow
-	$('.start-arrow').click(function() {
-		$('.part1').animatescroll();
-	});
 
+
+
+	var sTitle = $('.sketch-title').offset().top;
+
+	var part1 = $('.part1').offset().top;
+	var part2 = $('.part2').offset().top;
+	var part3 = $('.part3').offset().top;
+	var part4 = $('.part4').offset().top;
+	var part5 = $('.part5').offset().top;
+	var part6 = $('.part6').offset().top;
+	var part7 = $('.part7').offset().top;
+
+// start-arrow
+	$('.menu-about').click(function() {
+		if ($(window).scrollTop() > part1) {
+			$('.part1').animatescroll();
+		} else {
+			$('.part1').animatescroll({padding:75});
+		}
+	});
+	$('.start-arrow').click(function() {
+		if ($(window).scrollTop() > part1) {
+			$('.part1').animatescroll();
+		} else {
+			$('.part1').animatescroll({padding:75});
+		}
+	});	
+// Scroll Effect
+	$(window).scroll(function(){
+		var wScroll = $(this).scrollTop();
+		// console.log(wScroll);
+		
+		// TITLE OPACITY
+			var sTitleNew = $('.sketch-title').offset().top;
+			var k = (sTitleNew -sTitle) /5;
+			if (k < 1) {
+				k = 1;
+			}
+			var opacity = 1 / k;
+			$('.sketch').css('transform','translate(0px, '+ wScroll /4 +'%)');
+
+			$('.sketch-title').css({
+				'transform': 'translate(0px, '+ wScroll /4 +'%)',
+				'opacity': opacity
+			});
+			$('.sub-title').css({
+				'transform': 'translate(0px, '+ wScroll /4 +'%)',
+				'opacity': opacity
+			});
+
+		// MENU
+			if ( wScroll >= $('.start-arrow').offset().top) {
+				$('.menu-btn').css('position','fixed').addClass('animated bounce');
+			} else {
+				$('.menu-btn').css('position','relative').removeClass('animated bounce');
+			}
+
+	});
